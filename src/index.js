99a45611b200c01cea689e45993728015420c475
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-08 14:37:09 +0100

    `prop-types` `react` `react-dom`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import CheckAnswersSprocket from './sprockets/check-answers'
+import ErrorSummarySprocket from './sprockets/error-summary'
+import FieldsetSprocket from './sprockets/fieldset'
+
 export CheckAnswersSprocket from './sprockets/check-answers'
 export ErrorSummarySprocket from './sprockets/error-summary'
 export FieldsetSprocket from './sprockets/fieldset'
+
+export default {
+  CheckAnswersSprocket,
+  ErrorSummarySprocket,
+  FieldsetSprocket
+}
