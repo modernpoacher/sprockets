ed356db29dd56a0ea3b48884f9c39107cbe54d76
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-10-21 17:45:09 +0100

    `fs` and `react@16.14.0`

--- build/gulp/transform/index.js
+++ build/gulp/transform/index.js
@@ -13,7 +13,7 @@ import handleWatchError from 'build/gulp/handle-watch-error'
 import {
   readFile,
   writeFile
-} from 'sacred-fs'
+} from 'fs/promises'
 
 const log = debug('shinkansen-sprockets:build:gulp')
 const STYLESHEET = /(<style type="text\/css">)[ -~\w\d"'+-:;,#%{}/*\n\s✓•]*(<\/style>)/gm
