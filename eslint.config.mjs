9c60df248a5bc2cc16b9cabb0060301540f9e38e
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-01-10 13:30:24 +0000

    ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,6 +52,11 @@ const reactSettings = {
 }
 
 export default [
+  {
+    ignores: [
+      'coverage'
+    ]
+  },
   /**
    *  React config for all `jsx` and `tsx` files
    */
@@ -102,6 +107,10 @@ export default [
       'src/**/*.tsx',
       'stories/**/*.jsx'
     ],
+    ignores: [
+      'src/**/__tests__/**/*.tsx',
+      'stories/**/__tests__/**/*.jsx'
+    ],
     languageOptions: {
       parser: babelParser,
       parserOptions: {
@@ -110,6 +119,36 @@ export default [
       },
       globals: {
         ...globals.browser
+      }
+    },
+    plugins: {
+      ...reactPlugins,
+      ...storybookPlugins
+    },
+    rules: {
+      ...reactRules
+    },
+    settings: {
+      ...reactSettings,
+      'import/resolver': {
+        'babel-module': {}
+      }
+    }
+  }),
+  standard({
+    files: [
+      'src/**/__tests__/**/*.tsx',
+      'stories/**/__tests__/**/*.jsx'
+    ],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ...reactParserOptions,
+        project: null
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.jest
       }
     },
     plugins: {
@@ -160,6 +199,9 @@ export default [
     files: [
       'src/**/*.tsx'
     ],
+    ignores: [
+      'src/**/__tests__/**/*.tsx'
+    ],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
@@ -169,6 +211,32 @@ export default [
       },
       globals: {
         ...globals.browser
+      }
+    },
+    plugins: {
+      ...reactPlugins
+    },
+    rules: {
+      ...reactRules
+    },
+    settings: {
+      ...reactSettings
+    }
+  }),
+  typescript({
+    files: [
+      'src/**/__tests__/**/*.tsx'
+    ],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ...reactParserOptions,
+        projectService: true,
+        project: 'tsconfig.json'
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.jest
       }
     },
     plugins: {
