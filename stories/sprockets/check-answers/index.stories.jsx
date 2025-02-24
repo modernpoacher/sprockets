8012876c12d95db11ea82ce9cccaf432fa5a6637
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-02-24 16:00:58 +0000

    React Router

--- stories/sprockets/check-answers/index.stories.jsx
+++ stories/sprockets/check-answers/index.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
   MemoryRouter
-} from 'react-router-dom'
+} from 'react-router'
 
 import CheckAnswersSprocket from '#sprockets/sprockets/check-answers'
 
--- stories/sprockets/error-summary/index.stories.jsx
+++ stories/sprockets/error-summary/index.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
   MemoryRouter
-} from 'react-router-dom'
+} from 'react-router'
 
 import ErrorSummarySprocket from '#sprockets/sprockets/error-summary'
 
